ad6b6720b4921f35bbc25e25e4285635646d7308
MrPomme <damien.tournie@edu.itescia.fr>
2020-11-23 11:10:04 +0100

    Création du Tir et destruction des balles + Son de tire

---
+++ New Unity Project/Assets/Script/Tir.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tir : MonoBehaviour
+{
+    public GameObject Projectile;
+    public int Force = 50;
+    public AudioClip SoundTir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(SoundTir);
+            GameObject Bullet = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
+            Bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward) * Force;
+            Destroy(Bullet, 2f);
+
+        }
+    }
+
+}
