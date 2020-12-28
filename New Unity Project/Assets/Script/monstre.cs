7d7eb3b5514ba0fca66212c955235d3d8a293809
MrPomme <damien.tournie@edu.itescia.fr>
2020-12-28 13:31:11 +0100

    Correction bug  + Menu + camera

--- New Unity Project/Assets/Script/monstre.cs
+++ New Unity Project/Assets/Script/monstre.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.AI;
 
 public class monstre : MonoBehaviour
 {
     public Transform Target;
     public UnityEngine.AI.NavMeshAgent agent;
-    public int VieEnemie = 50; 
+    public int VieEnemie = 50;
+
+    private Animation animator;
+    private bool isDistanceCheck = false;
+    private float timeLeft = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = gameObject.GetComponent<Animation>();
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
         Target = GameObject.Find("Player").GetComponent<Transform>();
         
@@ -28,8 +34,26 @@ public class monstre : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(transform.position, Target.position);
+
         agent.destination = Target.position;
+        if (distance <= 3)
+        {
+            if (!isDistanceCheck)
+                isDistanceCheck = true;
+            else
+                timeLeft -= Time.deltaTime;
+            if(timeLeft <= 0.0f)
+            {
+                //attack
+               // animator.SetBool("Attack", true);
+            }
+            
+        }
+
         if (VieEnemie <= 0)
             Destroy(gameObject);
+
+
     }
 }
